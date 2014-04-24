739fe930974f6e4c13e46367aa0d17801a08898b
Ezra Miller <emiller@lsoc.org>
2014-04-23 20:33:50 -0500

    test

---
+++ js/main.js
@@ -0,0 +1,132 @@
+var opacity = 0.1;
+var color = 'rgba(255,255,255, ' + opacity + ')';
+var canvas = document.getElementById('canvas'),
+    ctx = canvas.getContext('2d'),
+    positionsX = [],
+    positionsY = [],
+    val = 50;
+
+var hammertime = new Hammer(canvas);
+// hammertime.on("drag", function(ev){
+//   ev.gesture.preventDefault();
+//   alert("Cool!");
+// })
+
+ctx.canvas.width  = window.innerWidth;
+ctx.canvas.height = window.innerHeight;
+function resize_canvas(){
+    if (canvas.width  < window.innerWidth || canvas.width  > window.innerWidth){
+        canvas.width  = window.innerWidth;
+    }
+    if (canvas.height < window.innerHeight || canvas.height > window.innerHeight ){
+        canvas.height = window.innerHeight;
+    }
+}
+$(window).resize(resize_canvas);
+(function () {
+  function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+      return {
+        x: evt.gesture.touches[0].clientX - rect.left,
+        y: evt.gesture.touches[0].clientX - rect.top
+    };
+  }
+
+
+
+  hammertime.on('drag',function(evt){
+    evt.gesture.preventDefault();
+    var mousePos = getMousePos(canvas,evt);
+    var x = mousePos.x;
+    var y = mousePos.y;
+    drawLines(x,y);
+  }, false);
+  function drawLines(x,y){
+    positionsX.push(x);
+    positionsY.push(y);
+    for (var i = 0; i < positionsX.length; i++) {
+      var dist = distance(positionsX[i], positionsY[i], x,y);
+      var r = Math.random()*100;
+      if(dist < val && r < val){
+      //1-dist/val;
+        ctx.beginPath();
+        ctx.moveTo(positionsX[i],positionsY[i]);
+        ctx.lineTo(x,y);
+        ctx.strokeStyle = color;
+        ctx.stroke();
+      }
+    }
+  }
+
+  function distance(x1, y1, x2, y2){
+    var dist = Math.sqrt(((x2-x1)*(x2-x1)) + ((y2-y1)*(y2-y1)));
+    return dist;
+  }
+}());
+
+var flag=true;
+$(document).ready(function(){
+  $('p').hide();
+  $('#box').hide();
+  $('.nav_link').click(function(){
+        $('a').removeClass('link-clicked');
+
+    $(this).addClass('link-clicked');
+    if(flag){
+    $('#box').show();
+    $('#box').animate({
+      border:"solid 2px",
+      height: "+=400",
+      width: "+=800"
+    },"fast" );
+    flag = false;
+  }
+
+
+
+
+    if($(this).text() === "WORK"){
+      $('p').hide();
+      $('#work-p').show();
+    }
+    if($(this).text() === "BLOG"){
+      $('p').hide();
+      $('#blog-p').show();
+    }
+    if($(this).text() === "ABOUT"){
+      $('p').hide();
+      $('#about-p').show();
+    }
+  });
+  $('canvas, #box').click(function(){
+    
+    if(!flag){
+    $('p').hide();
+    $('#box').animate({
+      height: "-=400",
+      width: "-=800"
+    },"fast" );
+    flag = true;
+    }
+  }).css();
+
+
+});
+  
+var colors = ['rgba(255,255,255, ','rgba(255,0,0, ','rgba(0,255,0, ','rgba(0,0,255, '];
+console.log(colors[0]);
+$('.white').addClass('white-clicked')
+$('#colors div').each(function(index){
+   var col = colors[index];
+    $(this).click(function(){
+      $('#colors div').removeClass('clicked');
+      $('#colors div').removeClass('white-clicked');
+      if(!$(this).hasClass('white')){
+        $(this).addClass('clicked')
+      } else{
+        $(this).addClass('white-clicked')
+      }
+      color =  col + opacity + ')';
+    })
+})
+
---
+++ js/webcam2.js
@@ -0,0 +1,116 @@
+var video = document.querySelector("#videoElement");
+var canvas = document.querySelector("#canvas");
+var ctx = canvas.getContext('2d');
+var width = 500;
+var height = 375;
+canvas.width = width;
+canvas.height = height;
+
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
+ 
+if (navigator.getUserMedia) {       
+    navigator.getUserMedia({video: true}, success, failure);
+}
+ 
+function success(stream) {
+    video.src = window.URL.createObjectURL(stream);
+    video.play();
+     //setup(stream);
+    setInterval(function(){draw();},500);
+}
+ 
+function failure(e) {
+    console.log("nope lol")
+}
+
+var frames = [];
+var barWidth = 3;
+
+//function setup(stream){
+
+  // video.onloadeddata = function(e) {
+  //   ctx.drawImage(video, 0, 0);  
+  //   videoPixels = ctx.getImageData(0,0,width,height)
+  //   videoPixelData = videoPixels.data;
+  //   imagePixels = ctx.createImageData(width,height);
+  //   imagePixelData = imagePixels.data;
+  //   for (var i = 0; i<videoPixelData.length; i++){
+  //     videoPixelData[i] = imagePixelData[i];
+  //   }
+  //   console.log(imagePixelData)
+  //   frames.push(imagePixels);
+    
+  //   if (frames.length > width/4) {
+  //     frames.shift();
+  //   }
+//  };
+//}
+function draw(){
+
+  var frame = readFrame();
+  
+  var imagePixels = ctx.createImageData(width,height);
+  
+  for(var i = 0; i < frame.data.length; i++){
+    imagePixels.data[i] = frame.data[i];
+  }
+
+  frames.push(imagePixels);
+  // if(frames.length > width/4){
+  //   frames.shift();
+  // }
+  //var currentImage = 0;
+
+  // for (var y = 0; y < height; y+=barWidth) {
+  //   if(currentImage < frames.length){
+  //     var img = frames[currentImage];
+  //     if (img != null){
+  //       for(var x = 0; x < width; x++){
+  //         for(var i = 0; i < barWidth; i++){
+  //             var location = (x + (y + i) * width)*4
+  //             frame.data[location] = img.data[location];
+  //             frame.data[location+1] = img.data[location+1];
+  //             frame.data[location+2] = img.data[location+2];
+  //             frame.data[location+3] = img.data[location+3];
+  //         }
+  //       }
+  //     } 
+  //       currentImage++;
+  //   } else {
+  //       break;
+  //   }  
+  // }
+var currentImage = 0;
+  for(var x = 0; x < width; x++){
+    for(var y = 0; y < height; y++){
+          var img = frames[currentImage];
+
+              var location = (x + (y * width))*4
+              frame.data[location] = img.data[location];
+
+              frame.data[location+1] = img.data[location+1];
+              frame.data[location+2] = img.data[location+2];
+              frame.data[location+3] = img.data[location+3];
+      
+          }
+  }
+  currentImage++;
+    ctx.putImageData(frame,0,0);
+  
+  //window.requestAnimationFrame(draw);
+}
+
+function readFrame() {
+    try {
+      ctx.drawImage(video, 0, 0, width, height);
+    } catch (e) {
+      // The video may not be ready, yet.
+      return null;
+    }
+
+    return ctx.getImageData(0, 0, width, height);
+}
+
+
+//constructor thing  http://www.phpied.com/canvas-pixels-3-getusermedia/
+
